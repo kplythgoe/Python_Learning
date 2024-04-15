60918c8e7550a8ce9f67d01897a1a095ef4fe86b
Kieran Lythgoe <91622486+kplythgoe@users.noreply.github.com>
2024-04-15 13:05:51 +0100

    Add files via upload
    The rain notification project sends a notification via email to myself every morning at 7am to let me know whether or not it will rain that day (in the next 12 hours). This is done by utilising an API key from openweathermap.org. From here I was able to use one of their API keys to dig through the dictionary or data and send a message to my own email (using smtplib) depending on whether or not it was going to rain that day, according to the weather.

---
+++ j_day35_rain_notification.py
@@ -0,0 +1,41 @@
+import requests
+import smtplib
+
+my_email = "kieranplythgoe@gmail.com"
+password = "***************"
+
+parameters = {
+    "lat": 53.398640,
+    "lon": -2.653330,
+    "appid": "9301dceac37b1adf05ec2d1b93b7521e",
+    "cnt": 4
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+three_hours_gaps = data["list"]
+
+
+def will_it_rain():
+    for gap in three_hours_gaps:
+        for weather in gap["weather"]:
+            if weather["id"] < 700:
+                return True
+
+
+will_rain = will_it_rain()
+weather_message = ""
+
+if will_rain:
+    weather_message = "According to the weather, looks like we're in for some rain today. Grab an umbrella!"
+else:
+    weather_message = "According to the weather, looks like it isn't going to rain today!"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="kieranplythgoedev@outlook.com",
+                        msg=f"Subject:Weather Update\n\n{weather_message}")
+
